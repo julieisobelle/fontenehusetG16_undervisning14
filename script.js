9d34b46c23c341cc1bf1919294d0e3605e66510f
julieisobelle <julie.kodehode@gmail.com>
2025-12-18 12:35:48 +0100

    Added comments explaining

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+/* Ordbok:
+const er en konstant variabel som ikke kan bytte verdi
+document refererer til HTML dokumentet
+.querySelector("#") henter et element med en spesifik id med # symbolet i string
+.getElementById("") henter et spesifikt element med id
+.addEventListener("click", function () {}) setter opp en hendelse som lytter etter et museklikk og kjører en anonym funksjon
+.style manipulererer CSS med inline styling som for eksempel da display = flex
+.classList.toggle("") ser gjennom en liste av de registrerte klassenavnene i koden og skrur av/på navnet gitt i string
+*/
+
+// Sjekker komunikasjon mellom index.html og script.js filene
 console.log("Hello World");
 
 // 1. Henter hamburger ikonet fra HTML
-const hamburgerToggle = document.getElementById("hamburgerToggle");
-//                              .querySelector("#hamburgerToggle");
+const hamburgerToggle = document.querySelector("#hamburgerToggle");
+// Alternativ metode:           .getElementById("hamburgerToggle");
+
+// Logger ut at vi henter riktig HTML element før vi går videre
 console.log(hamburgerToggle);
 
 // 2. Legger til en hendelse aktivtert med et museklikk
@@ -23,24 +36,32 @@ hamburgerToggle.addEventListener("click", function () {
 //
 //
 
+// 1.
 const dropDownOne = document.querySelector("#dropDownOne");
 console.log(dropDownOne);
 
+// 2.
 dropDownOne.addEventListener("click", function () {
   console.log("Clicked?");
 
+  // 3.
   const showTextOne = document.querySelector("#showTextOne");
 
+  // 4.
   showTextOne.classList.toggle("visible");
 });
 
+// 1.
 const dropDownTwo = document.querySelector("#dropDownTwo");
 console.log(dropDownOne);
 
+// 2.
 dropDownTwo.addEventListener("click", function () {
   console.log("Clicked?");
 
+  // 3.
   const showTextTwo = document.querySelector("#showTextTwo");
 
+  // 4.
   showTextTwo.classList.toggle("visible");
 });
